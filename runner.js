12a6023aea89fa116c643ce06a56731050579094
Lucas Holmquist <lholmqui@redhat.com>
2017-07-18 18:57:36 +0000

    playing around with this kube-client and different way of getting the config

--- runner.js
+++ runner.js
@@ -6,6 +6,11 @@ const options = {
   }
 };
 
+const Api = require('kubernetes-client');
+const core = new Api.Core(Api.config.getInCluster());
+
+console.log(core);
+
 nodeshift.deployApplication(options).then((message) => {
   console.log('Application Depoyed');
 }).catch((err) => {
